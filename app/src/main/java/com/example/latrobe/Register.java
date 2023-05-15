94626b47f4326730b0e53396547c0f14a8b6ef51
mayank <mayankrajput392@gmail.com>
2023-05-15 19:54:08 +0530

    Initial Commit and Login Page

---
+++ app/src/main/java/com/example/latrobe/Register.java
@@ -0,0 +1,93 @@
+package com.example.latrobe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Register extends AppCompatActivity {
+    EditText em,pd;
+    Button re;
+    FirebaseAuth mAuth;
+    TextView cl;
+
+
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            startActivity(new Intent("com.example.Home"));
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+        mAuth= FirebaseAuth.getInstance();
+        em=findViewById(R.id.editTextTextPersonName);
+        pd=findViewById(R.id.editTextTextPassword);
+
+        cl= findViewById(R.id.textView3);
+        cl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent("com.example.MainActivity"));
+            }
+        });
+
+
+        re=findViewById(R.id.button);
+
+        re.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email,password;
+                email= String.valueOf(em.getText());
+                password= String.valueOf(pd.getText());
+                
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(Register.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(Register.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Register.this, "Account Created",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Register.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+
+            }
+        });
+    }
+}
